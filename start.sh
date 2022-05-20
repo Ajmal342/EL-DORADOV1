89cfd908e5f095eded92f0f17d49fdf5fa21f55d
A-J-M-A-L <88123331+Ajmal342@users.noreply.github.com>
2022-05-20 08:46:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/Ajmal342/EL-DORADOV1.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EL-DORADOV1
+  git clone $UPSTREAM_REPO /Evamaria
 fi
-cd /EL-DORADOV1
+cd /Evamaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
